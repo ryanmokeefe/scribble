b7bbe9f12f47ca16a0a03cc6f735274a4f5e42fd
Andy <andrew.sunglae.kim@gmail.com>
2015-11-11 12:48:06 -0500

    refactor create action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,21 +6,13 @@ class TagsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @category = Category.find_by(category_params)
-    if Tag.find_by(post: @post, category: @category)
-    else
-      @tag = @post.tags.create
-      if @tag.category = @category
-      else
-        @tag.category = Category.create(category_params)
-      end
-      @tag.save
+    @category = Category.find_or_create_by(name: params[:category_name])
+
+    existing_tag = Tag.find_by(post: @post, category: @category)
+    unless existing_tag
+      @post.tags.create(category: @category)
     end
     redirect_to post_path(@post)
   end
 
-  private
-  def category_params
-    params.require(:category).permit(:name)
-  end
 end
