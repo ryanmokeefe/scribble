e440ac683367f3151273ba61c91dec3eb36e8c40
Jesse Shawl <jesse@jshawl.com>
2015-11-11 11:54:40 -0500

    fix foreign key constraint

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
   has_many :comments
-  has_many :tags
+  has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
   belongs_to :user
 end
