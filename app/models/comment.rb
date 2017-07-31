6c621138fa055a826356dbce416805cf0260380b
Andrew Whitley <awhitley1233@gmail.com>
2017-07-31 15:30:07 -0400

    update to Rails 5.0.2 / Spring

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class Comment < ActiveRecord::Base
+class Comment < ApplicationRecord
   default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::Base
+class Post < ApplicationRecord
   default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
   has_many :comments
 end
